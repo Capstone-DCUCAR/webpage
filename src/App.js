adc40a3c0fe2b141efcba81dfefde4348dc32434
jhw296 <johyewon4601@gmail.com>
2023-04-10 16:45:39 +0900

    강의실 상세 내용 추가

--- src/App.js
+++ src/App.js
@@ -12,6 +12,16 @@ function App() {
   const [modalContent, setModalContent] = useState("");
   const [selectedRoomNumber, setSelectedRoomNumber] = useState("");
 
+  const classes = {
+    501: '강의실', 502: '강의실', 503: '강의실', 504: '프로그래밍실습실1', 505: '강의실', 506: '학생회실', 507: '소프트웨어 스타트업 동아리실', 508: '오픈소스 소프트웨어 동아리실', 
+    509: '소프트웨어 창작 동아리실', 510: '', 511: '소프트웨어 세미나실', 512: '인지정보처리시스템 연구실', 513: '모바일멀티미디어 연구실', 514: '이동인터넷 연구실', 515: '소프트웨어융합대학 학장실', 
+    516: '컴퓨터소프트웨어 학부 사무실', 517: '컴퓨터네트워크보안 실습실', 518: 'HCI 실습 준비실', 519: '데이터베이스시스템 연구실', 520: '프로그래밍실습실2', 521: '소프트웨어 융합실습실', 
+    "521-1": '서동만 교수님 연구실', "521-2": '김미혜 교수님 연구실', 522: '전수빈 교수님 연구실', 523: '김병창 교수님 연구실', 524: '이종학 교수님 연구실', 525: '지능SW컴퓨팅 심화실습실', 
+    526: 'IoT 심화실습실', 529: '변태영 교수님 연구실', 530: '배인한 교수님 연구실', 531: '조용현 교수님 연구실', 532: '김행곤 교수님 연구실', 533: '소프트웨어공학 연구실', 534: '소프트웨어공학 실습실', 
+    535: '모바일멀티미디어 실습실', 536: '첨단표준형 임베디드 실습실', 539: '현장 미러형 실습실', 540: '강의실', 541: 'LINC+ 동아리실', 551: '신정훈 교수님 연구실', 552: '길준민 교수님 연구실', 
+    553: '이경률 교수님 연구실', 554: '김기성 교수님 연구실', 555: '메카트로닉스공학 준비실', 556: '강의실', 557: '강의실', 558: '사이버보안 소프트웨어 실습실', 559: 'IoT 프로그래밍 실습실' 
+  };
+  
   const buttons = [
     ...Array.from({ length: 21 }, (_, i) => ({
       label: `${501 + i}호`,
@@ -56,7 +66,7 @@ function App() {
     const index = roomNumbers.indexOf(roomNumber);
     setSelectedRoomNumber(roomNumber);
     setModalContent(
-      `선택하신 목적지는 ${roomNumber}호 입니다. 해당 목적지로 안내를 원하시면 안내시작 버튼을 눌러주세요.`
+      `선택하신 목적지는 ${roomNumber}호 ${classes[roomNumber]}입니다.`
     );
     setModalIsOpen(true);
 
@@ -84,7 +94,7 @@ function App() {
         <div class="mainimgStyle">
           <h1>안내를 원하는 장소를 선택하세요</h1>
           <div class="contentStyle">
-            <img src={imageSrc} alt="snslab" />
+            <img src={imageSrc} alt="snslab" className="img"/>
             <div class="buttonsStyle">
               {buttons.map((button) => (
                 <button
@@ -93,6 +103,7 @@ function App() {
                   onClick={() => handleButtonClick(button.roomNumber)}
                 >
                   {button.label}
+        
                 </button>
               ))}
             </div>
@@ -106,7 +117,10 @@ function App() {
             안 내
             <button onClick={closeModal}>x</button>
           </header>
-          <main>{modalContent}</main>
+          <main>
+            {modalContent}
+            <br/> 해당 목적지로 안내를 원하시면 안내시작 버튼을 눌러주세요.
+            </main>
           <footer>
             <button onClick={handleStartGuide}>안내시작</button>
             <button onClick={closeModal}>닫기</button>
